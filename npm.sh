93f5b983a85e95f528cf682cd569b1e62c56198a
kuschti <kuschti@gmail.com>
2019-09-02 14:09:32 +0200

    update status comments

--- npm.sh
+++ npm.sh
@@ -4,3 +4,5 @@ npm i -g fkill-cli
 npm i -g http-server
 npm i -g npm-check
 npm i -g trash-cli
+
+fancy_echo "Finish npm"
--- symlinks.sh
+++ symlinks.sh
@@ -1,4 +1,6 @@
+fancy_echo "Start symlinks"
 ## Symlinks
 # Sublime Text 3
 # ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
 # ln -s "$HOME/Dropbox/AppSync/ST3/User" "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+fancy_echo "Finish symlinks"
